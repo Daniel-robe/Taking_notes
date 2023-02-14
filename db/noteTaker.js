e41732265d1e166ebc323bff421fac748f537dab
Daniel R <110840284+Daniel-robe@users.noreply.github.com>
2023-02-14 00:51:36 -0600

    Added routes files and js to edit the server json

---
+++ db/noteTaker.js
@@ -0,0 +1,40 @@
+const util = require('util');
+const fs = require('fs');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class NoteTaker {
+    readNotes() {
+        return readFileAsync('db/db.json');
+    }
+
+    writeNotes(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+
+    getNotes() {
+        let notesList = [];
+
+        this.readNotes().then((notes) => {
+            notesList.concat(JSON.parse(notes));
+        })
+
+        return notesList
+    }
+
+    addNote(note) {
+        const {title, text} = note;
+
+        if(!title){
+            throw new Error("Note title cant be empty");
+        } if (!text){
+            throw new Error("Note text cant be empty");
+        }
+
+        newNote = {title, text}
+
+        return this.getNotes().then((notes) => [...notes, newNote])
+            .then((newNotes) => this.writeNotes(newNotes)).then(() => newNote);
+    }
+}
